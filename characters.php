b6e9e0e1cbbad48ddb5d208626eeaeaf91e049eb
kgkaye <70980732+kgkaye@users.noreply.github.com>
2021-12-04 11:18:56 -0800

    Load characters and info into html

--- characters.php
+++ characters.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Simpsons Archives</title>
+    <title>Simpsons Archive</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
@@ -17,20 +17,15 @@
 <div id="content" class="site-content">
     <div id="primary" class="content-area">
         <div id="main" class="site-main">
-        
             <div class="form__container layout-container">
                 <div class="form__row layout-row">
                     <div class="form__itemsContainer">
-
                         <div class="form__imageContainer">
                             <img src="images/simpsons.jpeg" alt="Simpsons" class="form__image">
                         </div>
-
                         <div class="form__card">
 
-                            <h3 class="form__heading">
-                                Select characters to show
-                            </h3>
+                            <h3 class="form__heading">Select characters to show</h3>                            
 
                             <form method="get">
 
@@ -69,9 +64,7 @@
                                 <input class="form__button" type="submit" value="Show Characters">
 
                             </form>
-
                         </div>
-
                     </div>
                 </div>
             </div>
@@ -79,46 +72,58 @@
             <div class="characters__container layout-container">
                 <div class="characters__row layout-row">
                     <ul class="characters__items">
-                        <li class="characters__itemContainer">
-                            <div class="characters__item">
-                                        
-                                <img src="images/homer.png" alt="homer" class="characters__image">
-                                        
-                                <div class="characters__info">
+                        
+                    <?php
 
-                                        <h3 class="characters__name">Homer Simpson</h3>                                                                                                    
-                                    <div class="characters__age characters__attribute">
-                                        <b>Age:</b> 40                                                
-                                    </div>
+    //connect and select
+    $dbc = mysqli_connect('localhost', 'root', '', 'simpsons_archive');
 
-                                    <div class="characters__occupation characters__attribute">
-                                        <b>Occupation:</b>  Nuclear Safety Inspector                                                                                                        
-                                    </div>
-                                    <div class="characters__voicedBy characters__attribute">
-                                        <b>Voiced by:</b> Dan Castellaneta                                                                                                        
-                                    </div>
+    //define query
+    $query = 'SELECT * FROM characters WHERE occupation IS NOT NULL';
 
-                                </div>
+    if ($r = mysqli_query($dbc, $query)) { //run the query
 
+        //retrieve and print every record
+    
+        while ($row = mysqli_fetch_array($r)) {     
+    ?>
+
+                <li class="characters__itemContainer">
+                    <div class="characters__item">                                
+                        <img src="<?= $row['image_url'] ?>" alt="<?= $row['id'] ?>" class="characters__image">
+                        <div class="characters__info">
+                                <h3 class="characters__name"><?= $row['first_name'] . ' ' . $row['last_name'] ?></h3> 
+                            <div class="characters__age characters__attribute">                          
+                                <b>Age:</b> <?= $row['age'] ?>                                                
                             </div>
-                        </li>
-                                                                                                                                                                                                                                                                                                                                            
-                    </ul>
-                </div>
-                </div>
+                            <div class="characters__occupation characters__attribute">
+                                <b>Occupation:</b> <?= $row['occupation'] ?>                                                                                                        
+                            </div>
+                            <div class="characters__voicedBy characters__attribute">
+                                <b>Voiced by:</b> <?= $row['voiced_by'] ?>                                                                                                        
+                            </div>
+                        </div>
+                    </div>
+                </li>                                                                                                                                                                                                                                                                                                                                            
 
-        </div>
-    </div>
-</div>
+    <?php
+        } 
+    }else {
+        print '<p> could not retrieve data because:<br>' . mysqli_error($dbc) . ' . </p>
+        <p> The query was being run was: ' . $query . '</p>';
+    }
+    mysqli_close($dbc); //close connection
+
+    ?>
+
+                    </ul> 
+                </div> 
+
+            </div> <!--end characters-container-->
+        </div> <!--end site-main-->
+    </div> <!--end content-area-->
+</div> <!--end site-content-->
 <?php
-
-// include('characters.json');
-$jsondata = file_get_contents('characters.json');
-$simpsons = json_decode($jsondata, true);
-// $characters = include('characters.json');
-// $simpsons = json_decode($characters, true);
-
-print_r($simpsons);
 
 ?>
 </body>
--- data.php
+++ data.php
@@ -1,28 +1,67 @@
-<?php
+<!doctype html>
+<html lang="en>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Simpsons Archive</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header id="masthead" class="site-header layout-container">
+        <a href="/">
+            <img class="site-header__logo" src="images/logo.svg" alt="Logo">
+        </a>
+    </header>
 
-//connect and select
-$dbc = mysqli_connect('localhost', 'root', '', 'simpsons_archive');
+    <div class="characters__container layout-container">
+        <div class="characters__row layout-row">
+            <ul class="characters__items">
+    <?php
 
-//define query
-$query = 'SELECT * FROM characters';
+    //connect and select
+    $dbc = mysqli_connect('localhost', 'root', '', 'simpsons_archive');
 
-if ($r = mysqli_query($dbc, $query)) { //run the query
+    //define query
+    $query = 'SELECT * FROM characters WHERE occupation IS NOT NULL';
 
-    //retrieve and print every record
-    while ($row = mysqli_fetch_array($r)) {
-        print "<p>{$row['first_name']}<br>
-        {$row['last_name']}<br>
-        {$row['age']}<br>
-        {$row['occupation']}<br>
-        {$row['voiced_by']}<br>
-        {$row['image_url']}</p>\n";
-    } 
-}else {
-    print '<p> could not retrieve data because:<br>' . mysql_error($dbc) . ' . </p>
-    <p> The query was being run was: ' . $query . '</p>';
-}
-mysqli_close($dbc); //close connection
+    if ($r = mysqli_query($dbc, $query)) { //run the query
 
+        //retrieve and print every record
+    
+        while ($row = mysqli_fetch_array($r)) {     
+    ?>
 
+                <li class="characters__itemContainer">
+                    <div class="characters__item">                                
+                        <img src="<?= $row['image_url'] ?>" alt="<?= $row['id'] ?>" class="characters__image">
+                        <div class="characters__info">
+                                <h3 class="characters__name"><?= $row['first_name'] . ' ' . $row['last_name'] ?></h3> 
+                            <div class="characters__age characters__attribute">                          
+                                <b>Age:</b> <?= $row['age'] ?>                                                
+                            </div>
+                            <div class="characters__occupation characters__attribute">
+                                <b>Occupation:</b> <?= $row['occupation'] ?>                                                                                                        
+                            </div>
+                            <div class="characters__voicedBy characters__attribute">
+                                <b>Voiced by:</b> <?= $row['voiced_by'] ?>                                                                                                        
+                            </div>
+                        </div>
+                    </div>
+                </li>                                                                                                                                                                                                                                                                                                                                            
 
-?>
+    <?php
+        } 
+    }else {
+        print '<p> could not retrieve data because:<br>' . mysqli_error($dbc) . ' . </p>
+        <p> The query was being run was: ' . $query . '</p>';
+    }
+    mysqli_close($dbc); //close connection
+
+    ?>
+            </ul> 
+        </div> 
+    </div> <!--end characters-container--> 
+
+</body>
+</html>
