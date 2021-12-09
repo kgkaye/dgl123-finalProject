e8921daa4dd2448246116a121b8e5bab82a34ad0
kgkaye <70980732+kgkaye@users.noreply.github.com>
2021-12-08 16:45:29 -0800

    If not character data line is omitted - ugly

--- characters.php
+++ characters.php
@@ -74,9 +74,9 @@
 
 include 'writeTable.php';
 
-    //create connection
+    //Create connection
     $conn = mysqli_connect('localhost', 'root', '', 'simpsons_archive');
-    //check connection
+    //Check connection
     if ($conn->connect_error) {
         die("Contection failed: " . $conn->connect_error);
     }
@@ -84,42 +84,52 @@ include 'writeTable.php';
     //GET USER CHECKED CHARACTERS FROM DB AND DISPLAY
     if (isset($_GET['character'])) {
 
+        //Create array to store characters chosen from checkboxes
         $selected = [];
         $selected =$_GET['character'];
         foreach ($selected as $key => $value) {
             
-                 //database query
+            //Get contents of table
             $query = 'SELECT * FROM characters';
             $results = mysqli_query($conn, $query);
             
             if ($results->num_rows > 0) {  
-            
                 while ($row = $results->fetch_assoc()) {
                 
                     if ($row['id'] == $key) {
-?>
 
-                    <li class="characters__itemContainer">
-                        <div class="characters__item">                                
-                            <img src="<?= $row['image_url'] ?>" alt="<?= $row['id'] ?>" class="characters__image">
-                            <div class="characters__info">
-                                    <h3 class="characters__name"><?= $row['first_name'] . ' ' . $row['last_name'] ?></h3> 
-                                <div class="characters__age characters__attribute">                          
-                                    <b>Age:</b> <?= $row['age'] ?>                                                
-                                </div>
-                                <div class="characters__occupation characters__attribute">
-                                    <b>Occupation:</b> <?= $row['occupation'] ?>                                                                                                        
-                                </div>
-                                <div class="characters__voicedBy characters__attribute">
-                                    <b>Voiced by:</b> <?= $row['voiced_by'] ?>                                                                                                        
-                                </div>
+                        print '<li class="characters__itemContainer">   
+                            <div class="characters__item">'; 
+
+                        if (!empty($row['image_url'])) {
+                            print "<img src=\"{$row['image_url']}\" alt=\" {$row['id']}\" class=\"characters__image\">";  
+                        }
+                        if (!empty($row['first_name']) && !empty($row['last_name'])) {
+                            print "<div class=\"characters__info\">
+                                <h3 class=\"characters__name\">{$row['first_name']} {$row['last_name']} </h3> ";
+                                    
+                        }
+                        if (!empty($row['age'])) {
+                            print "<div class=\"characters__age characters__attribute\">
+                            <b>Age:</b> {$row['age']}                                               
+                            </div>";
+                        }
+                            
+                        if (!empty($row['occupation'])) { 
+                        print "<div class=\"characters__occupation characters__attribute\">
+                            <b>Occupation:</b> {$row['occupation']}                                                                                                        
+                            </div>";
+                        }
+                        if (!empty($row['voiced_by'])) {
+                            print "<div class=\"characters__voicedBy characters__attribute\">
+                            <b>Voiced by:</b> {$row['voiced_by']}                                                                                                       
+                            </div>";
+                        }
+                        print '</div>
                             </div>
-                        </div>
-                    </li>                                                                                                                                                                                                                                                                                                                                            
-
-    <?php
+                            </li>';
+                        
                     }//end if
-
                 }//end while loop
 
             } else {
